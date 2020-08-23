da6bb96a6433179ebfc54bc5fe751b58b657cc4a
Souvik <souvikde.ns@gmail.com>
2020-08-23 23:05:24 +0530

    created login and signup action

---
+++ client/src/constants/api.endpoint.js
@@ -0,0 +1,4 @@
+const BASE_URL = ""
+
+export const LOGIN_URL = BASE_URL + '/api/auth/login'
+export const SIGNUP_URL = BASE_URL + '/api/auth/signup'
--- client/src/redux/actions/user.js
+++ client/src/redux/actions/user.js
@@ -2,6 +2,13 @@ import {
     SIGNIN
 } from '../../constants/action.type'
 
+import {
+    LOGIN_URL,
+    SIGNUP_URL
+} from '../../constants/api.endpoint'
+
+import Axios from 'axios'
+
 const signin = user => {
     return {
         type: SIGNIN,
@@ -9,3 +16,39 @@ const signin = user => {
     }
 }
 
+
+const login = (username, password) => {
+    return dispatch => {
+        Axios({
+            url: LOGIN_URL,
+            method: 'POST',
+            data: {
+                username: username,
+                password: password
+            }
+        }).then(res => {
+            dispatch(signin(res.data))
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+}
+
+const signup = (username, password) => {
+    return dispatch => {
+        Axios({
+            url: LOGIN_URL,
+            method: 'POST',
+            data: {
+                username: username,
+                password: password
+            }
+        }).then(res => {
+            dispatch(signin(res.data))
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+}
+
+export { login, signup }
